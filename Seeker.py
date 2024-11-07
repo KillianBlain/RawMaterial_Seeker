e725b9c8b1f01785b6493d1cc34c512092ca82d1
KillianBlain <killian.blain@hotmail.fr>
2024-11-07 09:19:14 +0100

    Update Seeker.py
    Langage switched to English. Added the possibility for the user to save the formulation or not.

--- Seeker.py
+++ Seeker.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 import csv
 
 def ingredient_research(cursor):
-    ingredient_property = str(input("Renseigner la propriété de l'ingrédient recherchée : ")) 
+    ingredient_property = str(input("Enter the researched property of your raw material : ")) 
     ingredient_property = f"%{ingredient_property}%" #f-string function to add % around ingredient_property
     cursor.execute("SELECT COSING_Ref_No, INCI_name FROM INGREDIENTS WHERE Chem_IUPACName_Description LIKE ?", (ingredient_property,))
     line = cursor.fetchone()
     while line:
         print(line)
         line = cursor.fetchone()
-    cosing = str(input("Renseigner le numéro de COSING choisi : "))
+    cosing = str(input("Enter the COSING number of the chosen raw material : "))
     return cosing
 
 def show_formulation (formulation, cursor) :
@@ -24,10 +24,10 @@ def show_formulation (formulation, cursor) :
         taille_tranches.append (ingredient [1])
         print ("- ", ingredient [1], "ml de ", elt [0], "(",elt [1], ") [", ingredient [0], "]")
         plt.pie(taille_tranches, labels = nom_tranches, autopct = "%1.1f%%")
-        plt.title("Composition de la formulation")
+        plt.title("Composition of the formulation")
         plt.show()
 
-def save_formulation (formulation, cursor, filename="formulation.csv"):
+def save_formulation (formulation, cursor, filename):
     with open(filename, mode='w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(["COSING_Ref_No", "INCI_name", "Function", "Volume (ml)"])
@@ -37,7 +37,7 @@ def save_formulation (formulation, cursor, filename="formulation.csv"):
             elt = cursor.fetchone()
             if elt:
                 writer.writerow([ingredient[0], elt[0], elt[1], ingredient[1]])
-    print(f"Formulation sauvegardée dans {filename}")
+    print("Formulation saved into",filename,".")
 
 connection_db = sqlite3.connect("raw_material.db")
 cursor = connection_db.cursor()
@@ -47,13 +47,17 @@ formulation = []
 i = True
 while i :
     cosing = ingredient_research(cursor)
-    v = float(input("Entrer le volume à utiliser : "))
+    v = float(input("Enter the desired volume (mL) : "))
     formulation.append([cosing, v])
-    i = str(input("Entrer 'oui' si vous souhaitez entrer une nouvelle MP ou 'non' si vous ne souhaitez plus en ajouter : "))
-    if i != "oui" :
+    i = str(input("Enter 'yes' if you want to enter a new raw materiel or 'no' if you doesn't want to add a new one : "))
+    if i != "yes" :
         i = False
 
 show_formulation (formulation, cursor)
-save_formulation (formulation, cursor)
+save = str(input("Enter 'yes' if you want to save you're formulation into a csv file or 'no' if you doesn't want to save : "))
+if save == "yes":
+    save_formulation (formulation, cursor, "formulation.csv")
+else:
+    print("formulation not saved.")
    
 connection_db.close()
